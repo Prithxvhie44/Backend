f495c4b325a8d32f6f6721d9c3dd460aa510b055
Prithxvhie44 <44prithviraj44@gmail.com>
2025-12-10 13:45:01 +0530

    Basic CRUD Operations

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+//To create data
+
 app.get('/create', async (req, res) => {
     let createduser=await userModel.create({
         name: "Prithviraj",
@@ -15,6 +17,32 @@ app.get('/create', async (req, res) => {
 
     })
     res.send(createduser);
+})
+
+//To update data
+app.get('/update',async(req,res)=>{
+    let updateduser=await userModel.updateOne({username:"prithvi123"},{name:"Prithviraj More"},{new:true})
+    res.send(updateduser);
+})
+
+//To fetch all data(This returns array of objects)
+
+app.get("/read", async(req,res)=>{
+    let users=await userModel.find();
+    res.send(users);
+})
+
+//To fetch specific data(This returns object of first matched data)
+
+app.get("/readone", async(req,res)=>{
+    let user=await userModel.findOne({username:"prithvi123"});
+    res.send(user);
+})
+
+//To delete data
+app.get("/delete", async(req,res)=>{
+    let deleteduser=await userModel.deleteOne({username:"prithvi123"});
+    res.send(deleteduser);
 })
 
 app.listen(3000);
